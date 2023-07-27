b121dd8c8539c5a0d650d2ce5aa2ce2ab3e7faf1
Inayah <inayahh.bawany@gmail.com>
2023-07-27 13:40:36 +0100

    Initial Commit

---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
