66502a25fefc980a4a2a7b77c6adfea5cb23d16b
Inayah <inayahh.bawany@gmail.com>
2023-08-01 09:31:27 +0100

    second comment

--- myapp/ContentView.swift
+++ myapp/ContentView.swift
@@ -6,20 +6,40 @@
 //
 
 import SwiftUI
+//COMMENT
 
 struct ContentView: View {
+    @State private var next = ""
+    @State private var titleText = "My name is Inayah, what about you?"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack (spacing: 75.0) {
+            Text(titleText)
+             
+            TextField("Type your name here..", text: $next)
+            Text("All about me")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+          
+            Image("proflePic")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+            
+            Spacer()
+            Button("Next") {
+                titleText = "Hello, \(next)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews:
+    PreviewProvider {
     static var previews: some View {
         ContentView()
     }
